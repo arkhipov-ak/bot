6e7eb2a764bdd6b1acbcb444b5241bef6f237ae7
Arkhipov <dev.arkhipov@gmail.com>
2025-04-03 23:49:59 +0300

    fix css

--- src/components/GameCanvas.tsx
+++ src/components/GameCanvas.tsx
@@ -56,6 +56,8 @@ export const GameCanvas: React.FC<GameCanvasProps> = ({
       
       // Рисуем самолет
       ctx.fillStyle = '#FFFFFF';
+
+      
       ctx.save();
       ctx.translate(airplane.x, airplane.y);
       ctx.beginPath();
@@ -119,10 +121,44 @@ export const GameCanvas: React.FC<GameCanvasProps> = ({
           return;
         }
         
-        ctx.fillStyle = '#FFFFFF';
-        ctx.beginPath();
-        ctx.arc(obstacle.x, obstacle.y, OBSTACLE_SIZE / 2, 0, Math.PI * 2);
-        ctx.fill();
+        function drawMeteor(
+          ctx: CanvasRenderingContext2D,
+          x: number,
+          y: number,
+          size: number
+        ) {
+          ctx.save();
+          
+          // Создаем радиальный градиент для основного тела метеорита
+          const gradient = ctx.createRadialGradient(x, y, size * 0.1, x, y, size * 0.5);
+          gradient.addColorStop(0, '#b0b2c0');
+          gradient.addColorStop(1, '#80838f');
+          ctx.fillStyle = gradient;
+          
+          // Рисуем основной круг метеорита
+          ctx.beginPath();
+          ctx.arc(x, y, size / 2, 0, Math.PI * 2);
+          ctx.fill();
+          
+          // Рисуем «кратеры»
+          ctx.fillStyle = '#343648';
+          ctx.beginPath();
+          ctx.arc(x - size * 0.2, y - size * 0.1, size * 0.1, 0, Math.PI * 2);
+          ctx.fill();
+          
+          ctx.beginPath();
+          ctx.arc(x + size * 0.1, y - size * 0.2, size * 0.07, 0, Math.PI * 2);
+          ctx.fill();
+          
+          ctx.beginPath();
+          ctx.arc(x + size * 0.15, y + size * 0.05, size * 0.05, 0, Math.PI * 2);
+          ctx.fill();
+          
+          ctx.restore();
+        }
+        
+        drawMeteor(ctx, obstacle.x, obstacle.y, OBSTACLE_SIZE )
+
       });
       
       // Удаление отмеченных снарядов и препятствий
